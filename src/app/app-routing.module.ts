94b910ef1c5fd8ff84c3c4a530a36ecc716bf494
Quartus Kok <quartusk@gmail.com>
2019-09-30 12:47:45 +0200

    Set up routing

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { TaskListComponent } from './task-list/task-list.component';
+
+const routes: Routes = [
+  { path: 'current', component: TaskListComponent },
+  { path: '', redirectTo: '/current', pathMatch: 'full'}
+]
+
+@NgModule({
+  declarations: [],
+  imports: [
+    RouterModule.forRoot(routes)
+  ],
+  exports: [
+    RouterModule
+  ]
+})
+export class AppRoutingModule { }
